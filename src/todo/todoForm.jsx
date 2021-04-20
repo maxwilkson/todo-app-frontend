205f5e2f76c51e5672dce2051c557e32585712f6
Max <maxwilkson@gmail.com>
2021-04-20 20:17:58 -0300

    Commit incial.

---
+++ src/todo/todoForm.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+import { add, changeDescription, search } from './todoActions'
+
+class TodoForm extends Component {
+
+    constructor(props) {
+        super(props)
+        this.keyHandle = this.keyHandle.bind(this)
+    }
+
+    componentDidMount() {
+        this.props.search()
+    }
+
+    keyHandle() {
+        const {add, search, description} = this.props
+        if (e.key === 'Enter') {
+            e.shiftKey ? search() : add(description)
+        } else if (e.key === 'Escape') {
+            this.props.handleClear()
+        }
+    }
+
+    render() {
+        const {add, search, changeDescription, description} = this.props
+        return (
+            <div role='form' className='todoForm'>
+                <Grid cols='12 9 10'>
+                    <input id='description' className='form-control'
+                        placeholder='Adicione uma tarefa'
+                        onChange={changeDescription}
+                        onKeyUp={this.keyHandle}
+                        value={description}></input>
+                </Grid>
+                <Grid cols='12 3 2'>
+                    <IconButton style='primary' icon='plus'
+                        onClick={() => add(description)}></IconButton>
+                    <IconButton style='info' icon='search'
+                        onClick={search}></IconButton>
+                    <IconButton style='default' icon='close'
+                        onClick={this.props.handleClear}></IconButton>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ description: state.todo.description })
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
---
+++ src/todo/todoReducer.js
@@ -0,0 +1,17 @@
+const INITAL_STATE = {
+    description: '',
+    list: []
+}
+
+export default (state = INITAL_STATE, action) => {
+    switch(action.type) {
+        case 'DESCRIPTION_CHANGED':
+            return { ...state, description: action.payload }
+        case 'TODOS_SEARCHED':
+            return { ...state, list: action.payload.data }
+        case 'TODO_ADDED':
+            return { ...state, description: ''}
+        default:
+            return state
+    }
+}
